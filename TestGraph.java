aeeda9422cf517bc8e126270ee7c271249c3dd1e
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-11-23 17:59:59 +0200

    Added a new node for testing

--- TestGraph.java
+++ TestGraph.java
@@ -11,16 +11,19 @@ class TestGraph {
 		
 		ArrayList<Node> parentsOfB = new ArrayList<Node>();
 		parentsOfB.add(g.get('A'));
-
 		g.add(new Node( 'B', parentsOfB, null ));
 
 		ArrayList<Node> parentsOfC = new ArrayList<Node>();
 		parentsOfC.add(g.get('A'));
-
 		ArrayList<Node> childrenOfC = new ArrayList<Node>();
 		childrenOfC.add(g.get('B'));
-
 		g.add(new Node('C', parentsOfC, childrenOfC ));
+
+		ArrayList<Node> parentsOfD = new ArrayList<Node>();
+		parentsOfD.add(g.get('B'));
+		ArrayList<Node> childrenOfD = new ArrayList<Node>();
+		childrenOfD.add(g.get('B'));
+		g.add(new Node('D', parentsOfD, childrenOfD ));
 
 		g.printGraph();
 	}
