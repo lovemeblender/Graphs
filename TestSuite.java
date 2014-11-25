cdd75c91dbef4bc89d578265360f6e7324be629b
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-11-25 09:48:25 +0200

    Add comments

--- TestSuite.java
+++ TestSuite.java
@@ -7,26 +7,26 @@ class TestGraph {
 		
 		Graph g = new Graph();
 
-		g.add(new Node( 'A', null, null ));
+		g.add(new Node( 'A', null, null )); // Create vertex A
 		
 		ArrayList<Node> parentsOfB = new ArrayList<Node>();
-		parentsOfB.add(g.get('A'));
-		g.add(new Node( 'B', parentsOfB, null ));
+		parentsOfB.add(g.get('A')); // A points to B
+		g.add(new Node( 'B', parentsOfB, null )); // Create vertex B
 
 		ArrayList<Node> parentsOfC = new ArrayList<Node>();
-		parentsOfC.add(g.get('A'));
+		parentsOfC.add(g.get('A')); // A points to C
 		ArrayList<Node> childrenOfC = new ArrayList<Node>();
-		childrenOfC.add(g.get('B'));
-		g.add(new Node('C', parentsOfC, childrenOfC ));
+		childrenOfC.add(g.get('B')); // C points to B
+		g.add(new Node('C', parentsOfC, childrenOfC )); // Create vertex C
 
 		ArrayList<Node> parentsOfD = new ArrayList<Node>();
-		parentsOfD.add(g.get('B'));
+		parentsOfD.add(g.get('B')); // B points to D
 		ArrayList<Node> childrenOfD = new ArrayList<Node>();
-		childrenOfD.add(g.get('B'));
-		g.add(new Node('D', parentsOfD, childrenOfD ));
+		childrenOfD.add(g.get('B')); // D points to B
+		g.add(new Node('D', parentsOfD, childrenOfD )); // Create vertex D
 
-		g.printGraph();
+		g.printGraph(); // Print adjacency list
 
-		g.DFS();
+		g.DFS(); // Traverse the graph DFS
 	}
 }
